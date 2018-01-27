aa4a3511c07d0075dcb470e0eb7fdeec3c04e407
Josh Simani <jsimani@gmail.com>
2018-01-27 02:19:53 -0500

    Added function contracts

--- Game.cpp
+++ Game.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #define DOWN 's'
 #define RIGHT 'd'
 
+// Different terminal escape characters for modifying text appearances
 #define NORMAL "\033[0m"
 #define BOLD "\033[1m"
 
@@ -34,6 +35,11 @@ typedef enum Space {
         BODY_FROM_LEFT, EMPTY, FOOD
 } Space;
 
+/* Constructor
+ * Purpose: Initialize members of the Game object
+ * Parameters: None
+ * Returns: Nothing
+ */
 Game::Game()
 {
         srand(time(NULL));
@@ -49,6 +55,13 @@ Game::Game()
         board = NULL;
 }
 
+/* Parameterized Constructor
+ * Purpose: Initialize members of the Game object with implementer's choice
+ *          of game board dimensions.
+ * Parameters: y_dimen (desired vertical size of board), x_dimen (desired 
+               horizontal size of board)
+ * Returns: Nothing
+ */
 Game::Game(int y_dimen, int x_dimen)
 {
         srand(time(NULL));
@@ -82,6 +95,12 @@ Game::Game(int y_dimen, int x_dimen)
         board[y_head][x_head] = HEAD;
 }
 
+/* Copy Constructor
+ * Purpose: Initialize members of the Game object based on the values from
+ *          another Game object.
+ * Parameters: source (Game object to be copied)
+ * Returns: Nothing
+ */
 Game::Game(const Game &source)
 {
         srand(time(NULL));
@@ -105,6 +124,12 @@ Game::Game(const Game &source)
         }
 }
 
+/* Assignment Overload "="
+ * Purpose: Overload the assignment operation (=) so that Games are not
+ *          shallow copied when assigned to each other.
+ * Parameters: source (Game object to be duplicated)
+ * Returns: Game (new Game object)
+ */
 Game &Game::operator=(const Game &source)
 {
         if (this == &source) {
@@ -144,6 +169,11 @@ Game &Game::operator=(const Game &source)
         return *this;
 }
 
+/* Destructor
+ * Purpose: Frees all heap-allocated memory in the Game object.
+ * Parameters: None
+ * Returns: Nothing
+ */
 Game::~Game()
 {
         if (board != NULL) {
@@ -156,8 +186,16 @@ Game::~Game()
                 delete[] board;
         }
         show_cursor();
+        cout << NORMAL;
 }
 
+/* run()
+ * Purpose: Runs the Snake game, generating the first "food," retreiving user 
+ *          input, and moving the Snake accordingly. Prints the board after
+ *          every move and ends the game when the Snake hits the wall.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::run()
 {
         hide_cursor();
@@ -165,6 +203,7 @@ void Game::run()
         print();
         cout << "Enter \'w\', \'a\', \'s\', or \'d\' to start!" << endl;
 
+        // Get initial input, do not start until a valid direction is provided
         do {
                 direction = getachar();
         } while (direction != UP && direction != DOWN && direction != LEFT &&
@@ -182,12 +221,20 @@ void Game::run()
         
 }
 
+/* get_move()
+ * Purpose: Gets a move from the user. If no move is provided, direction stays
+ *          the same as the previous direction.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::get_move()
 {
         char temp;
         int wait = speed;
-        int opposite_direction = DOWN;
+        int opposite_direction;
 
+        /* Sets an opposite direction so that the user can't select to 
+         * turn the Snake around */
         switch (direction) {
                 case UP:
                         opposite_direction = DOWN;
@@ -205,6 +252,8 @@ void Game::get_move()
                         break;
         }
 
+        /* Keeps prompting for input until time runs out. This is the delay
+         * in between each of the Snake's moves. */
         while (wait > 0) {
                 temp = getacharnow(0);
                 if (temp != '\0' && (temp == UP || temp == DOWN || 
@@ -219,6 +268,12 @@ void Game::get_move()
         }
 }
 
+/* move()
+ * Purpose: Moves the Snake's head an body in the direction it is supposed to
+ *          go. Changes the direction of the body accordingly when it moves.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::move()
 {
         switch (direction) {
@@ -239,107 +294,160 @@ void Game::move()
         }
 }
 
+/* move_up()
+ * Purpose: Moves the Snake's head up one space, and changes its body 
+ *          positions and directions accordingly.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::move_up()
 {
         if (y_head - 1 < 0) {
+                // Case 1: Snake hits a wall
                 game_over = true;
                 return;
         } else if (board[y_head - 1][x_head] == BODY_FROM_UP ||
                    board[y_head - 1][x_head] == BODY_FROM_DOWN ||
                    board[y_head - 1][x_head] == BODY_FROM_LEFT ||
                    board[y_head - 1][x_head] == BODY_FROM_RIGHT) {
+                // Case 2: Snake hits its own body
                 game_over = true;
                 return;
         } else if (board[y_head - 1][x_head] == FOOD) {
+                // Case 3: Snake hits food
                 carry_body(y_head, x_head, BODY_FROM_UP, true);
                 board[y_head--][x_head] = BODY_FROM_UP;
                 board[y_head][x_head] = HEAD;
                 bake_food();
                 snake_size++;
         } else {
+                // Case 4: Snake doesn't hit anything
                 carry_body(y_head, x_head, BODY_FROM_UP, false);
                 board[y_head--][x_head] = BODY_FROM_UP;
                 board[y_head][x_head] = HEAD;
         }
 }
 
+/* move_down()
+ * Purpose: Moves the Snake's head down one space, and changes its body 
+ *          positions and directions accordingly.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::move_down()
 {
         if (y_head + 1 >= y_dimension) {
+                // Case 1: Snake hits a wall
                 game_over = true;
                 return;
         } else if (board[y_head + 1][x_head] == BODY_FROM_UP ||
                    board[y_head + 1][x_head] == BODY_FROM_DOWN ||
                    board[y_head + 1][x_head] == BODY_FROM_LEFT ||
                    board[y_head + 1][x_head] == BODY_FROM_RIGHT) {
+                // Case 2: Snake hits its own body
                 game_over = true;
                 return;
         } else if (board[y_head + 1][x_head] == FOOD) {
+                // Case 3: Snake hits food
                 carry_body(y_head, x_head, BODY_FROM_DOWN, true);
                 board[y_head++][x_head] = BODY_FROM_DOWN;
                 board[y_head][x_head] = HEAD;
                 bake_food();
                 snake_size++;
         } else {
+                // Case 4: Snake doesn't hit anything
                 carry_body(y_head, x_head, BODY_FROM_DOWN, false);
                 board[y_head++][x_head] = BODY_FROM_DOWN;
                 board[y_head][x_head] = HEAD;
         }
 }
 
+/* move_left()
+ * Purpose: Moves the Snake's head left one space, and changes its body 
+ *          positions and directions accordingly.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::move_left()
 {
         if (x_head - 1 < 0) {
+                // Case 1: Snake hits a wall
                 game_over = true;
                 return;
         } else if (board[y_head][x_head - 1] == BODY_FROM_UP ||
                    board[y_head][x_head - 1] == BODY_FROM_DOWN ||
                    board[y_head][x_head - 1] == BODY_FROM_LEFT ||
                    board[y_head][x_head - 1] == BODY_FROM_RIGHT) {
+                // Case 2: Snake hits its own body
                 game_over = true;
                 return;
         } else if (board[y_head][x_head - 1] == FOOD) {
+                // Case 3: Snake hits food
                 carry_body(y_head, x_head, BODY_FROM_LEFT, true);
                 board[y_head][x_head--] = BODY_FROM_LEFT;
                 board[y_head][x_head] = HEAD;
                 bake_food();
                 snake_size++;
         } else {
+                // Case 4: Snake doesn't hit anything
                 carry_body(y_head, x_head, BODY_FROM_LEFT, false);
                 board[y_head][x_head--] = BODY_FROM_LEFT;
                 board[y_head][x_head] = HEAD;
         }
 }
 
+/* move_right()
+ * Purpose: Moves the Snake's head right one space, and changes its body 
+ *          positions and directions accordingly.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::move_right()
 {
         if (x_head + 1 >= x_dimension) {
+                // Case 1: Snake hits a wall
                 game_over = true;
                 return;
         } else if (board[y_head][x_head + 1] == BODY_FROM_UP ||
                    board[y_head][x_head + 1] == BODY_FROM_DOWN ||
                    board[y_head][x_head + 1] == BODY_FROM_LEFT ||
                    board[y_head][x_head + 1] == BODY_FROM_RIGHT) {
+                // Case 2: Snake hits its own body
                 game_over = true;
                 return;
         } else if (board[y_head][x_head + 1] == FOOD) {
+                // Case 3: Snake hits food
                 carry_body(y_head, x_head, BODY_FROM_RIGHT, true);
                 board[y_head][x_head++] = BODY_FROM_RIGHT;
                 board[y_head][x_head] = HEAD;
                 bake_food();
                 snake_size++;
         } else {
+                // Case 4: Snake doesn't hit anything
                 carry_body(y_head, x_head, BODY_FROM_RIGHT, false);
                 board[y_head][x_head++] = BODY_FROM_RIGHT;
                 board[y_head][x_head] = HEAD;
         }
 }
 
+/* carry_body()
+ * Purpose: Recursive function to iterate through the body of the Snake, 
+ *          each body part's direction accordingly and moving on to the next
+ *          body part.
+ * Parameters: y_position (the y coordinate of the body part being changed), 
+ *             x_position (the x coordinate of the body part being changed), 
+ *             new_direction (indicated direction that the current body part
+ *             should now have), food (true if the head ate a food during the
+ *             current move)
+ * Returns: void
+ */
 void Game::carry_body(int y_position, int x_position, int new_direction, 
                       bool food)
 {
         if (y_position - 1 >= 0 && 
             board[y_position - 1][x_position] == BODY_FROM_DOWN) {
+                /* Case 1: The next part of the body is above the current 
+                 * location */
                 board[y_position][x_position] = new_direction;
                 carry_body(y_position - 1, x_position, BODY_FROM_DOWN, food);
                 return;
@@ -347,6 +455,8 @@ void Game::carry_body(int y_position, int x_position, int new_direction,
 
         if (y_position + 1 < y_dimension && 
             board[y_position + 1][x_position] == BODY_FROM_UP) {
+                /* Case 2: The next part of the body is below the current 
+                 * location */
                 board[y_position][x_position] = new_direction;
                 carry_body(y_position + 1, x_position, BODY_FROM_UP, food);
                 return;
@@ -354,6 +464,8 @@ void Game::carry_body(int y_position, int x_position, int new_direction,
 
         if (x_position - 1 >= 0 && 
             board[y_position][x_position - 1] == BODY_FROM_RIGHT) {
+                /* Case 3: The next part of the body is left of the current 
+                 * location */
                 board[y_position][x_position] = new_direction;
                 carry_body(y_position, x_position - 1, BODY_FROM_RIGHT, food);
                 return;
@@ -361,18 +473,28 @@ void Game::carry_body(int y_position, int x_position, int new_direction,
 
         if (x_position + 1 < x_dimension && 
             board[y_position][x_position + 1] == BODY_FROM_LEFT) {
+                /* Case 4: The next part of the body is right of the current 
+                 * location */
                 board[y_position][x_position] = new_direction;
                 carry_body(y_position, x_position + 1, BODY_FROM_LEFT, food);
                 return;
         }
 
+        
         if (food) {
+                // Case 4: Last part of the body, Snake ate food
                 board[y_position][x_position] = new_direction;
         } else {
+                // Case 5: Last part of the body, Snake didn't eat food
                 board[y_position][x_position] = EMPTY;
         }
 }
 
+/* print()
+ * Purpose: Print the board.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::print()
 {
         screen_clear();
@@ -425,6 +547,13 @@ void Game::print()
 
 }
 
+/* bake_food()
+ * Purpose: Generates a food item in a random space on the board, given that 
+ *          there is a space to put the food. If there are no spaces to put 
+ *          the food and/or the board is full, it does nothing.
+ * Parameters: None
+ * Returns: void
+ */
 void Game::bake_food()
 {
         int y_rand, x_rand;
@@ -432,15 +561,23 @@ void Game::bake_food()
                 return;
         }
 
+        /* Keep generating new coordinates on the board until an empty space
+         * is found */
         do {
                 y_rand = rand() % y_dimension;
                 x_rand = rand() % x_dimension;
         } while (board[y_rand][x_rand] != EMPTY);
 
         board[y_rand][x_rand] = FOOD;
+        // Spped up the movement of the snake if it is still above 20
         speed -= (speed > 20 ? 1 : 0);
 }
 
+/* check_win()
+ * Purpose: Checks to see if the user has won the game
+ * Parameters: None
+ * Returns: bool (true if the user has won the game)
+ */
 bool Game::check_win()
 {
         bool done = true;
@@ -461,6 +598,11 @@ bool Game::check_win()
         return won;
 }
 
+/* empty_spaces()
+ * Purpose: Checks to see if the board contains any empty spaces
+ * Parameters: None
+ * Returns: bool (true if the board contains any empty spaces)
+ */
 bool Game::empty_spaces()
 {
         bool empty = false;
@@ -476,6 +618,12 @@ bool Game::empty_spaces()
         return empty;
 }
 
+/* end_game()
+ * Purpose: Prints message to user based on if they won or lost. Prompts user
+ *          if they want to play again
+ * Parameters: None
+ * Returns: void
+ */
 void Game::end_game()
 {
         char response;
